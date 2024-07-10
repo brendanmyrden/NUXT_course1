a632890bcee0eb485d12795acf63eccb7804e31a
MYRDEN <79550484+brendanmyrden@users.noreply.github.com>
2024-07-09 20:50:30 -0400

    Nuxt confguration resolved

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from "path";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  alias: {
+        "@": resolve(__dirname, "/"),
+  },
+});
+
+//  compatibilityDate: '2024-04-03',
+//  devtools: { enabled: true }
